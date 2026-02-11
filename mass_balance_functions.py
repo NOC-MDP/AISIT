5cb0ab6a7c3e0b9c1be20a838f765e324b5daa0c
Archie Cable <acable@noc.ac.uk>
2026-02-11 11:27:30 +0000

    Bug fix on mass balance equations

--- mass_balance_functions.py
+++ mass_balance_functions.py
@@ -14,18 +14,19 @@ def mass_fraction(oxy_iso, sal):
 
     Returns
     -------
-    fir, fme, fsi: xarray.DataArray
+    fsw, fmw, fsi: xarray.DataArray
+
     """
     # Constants
-    sir = 34.88
+    ssw = 34.88
+    smw = 0
     ssi = 3
-    sme = 0
-    dir_ = 0.34
-    dme = -21
+    dsw = 0.34
+    dmw = -21
     dsi = 2.1  # Using values from Jones et al., 2008
 
     # Build K matrix
-    k = np.array([1, dir_, sir], [1, dme, sme], [1, dsi, ssi])
+    k = np.array([[1, 1, 1], [dsw, dmw, dsi], [ssw, smw, ssi]])
 
     # Take the inverse and separate into components
     kinv = np.linalg.inv(k)
@@ -34,8 +35,8 @@ def mass_fraction(oxy_iso, sal):
     a3, b3, c3 = kinv[2]
 
     # Compute the mass fractions
-    fir = a1 + b1 * oxy_iso + c1 * sal
-    fme = a2 + b2 * oxy_iso + c2 * sal
+    fsw = a1 + b1 * oxy_iso + c1 * sal
+    fmw = a2 + b2 * oxy_iso + c2 * sal
     fsi = a3 + b3 * oxy_iso + c3 * sal
 
-    return fir, fme, fsi
+    return fsw, fmw, fsi
